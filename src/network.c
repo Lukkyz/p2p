6710013e8ccc9329be97caa54a0a245eec911de4
Lukkyz <bienaimelucas@hotmail.fr>
2022-04-03 01:57:19 +0200

    Add threading

--- src/network.c
+++ src/network.c
@@ -1,3 +1,5 @@
+#include "network.h"
+
 #include <arpa/inet.h>
 #include <errno.h>
 #include <event2/buffer.h>
@@ -5,18 +7,23 @@
 #include <event2/listener.h>
 #include <event2/thread.h>
 #include <netdb.h>
+#include <pthread.h>
 #include <signal.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
+#include <unistd.h>
 
-#define SA struct sockaddr
-#define MAX_OUT_CONNECTION 10
+peer* peers[100];
+int peer_num = 0;
+
+void add_peer(peer* new_peer) {
+    peers[peer_num++] = new_peer;
+};
 
 struct sockaddr_in6 create_addr(int port, char* address) {
-    struct evconnlistener* listener;
     struct sockaddr_in6 addr;
 
     addr.sin6_family = AF_INET6;
@@ -38,6 +45,8 @@ void read_cb(struct bufferevent* bev, void* ctx) {
     struct evbuffer* input = bufferevent_get_input(bev);
     struct evbuffer* output = bufferevent_get_input(bev);
     evbuffer_remove(input, buff, 1024);
+    peer* new_peer = ctx;
+    printf("%ld", new_peer->timestamp);
     printf("%s\n", buff);
 }
 
@@ -49,14 +58,20 @@ void accept_conn_cb(struct evconnlistener* listener, evutil_socket_t fd,
     struct sockaddr_in6* conn_addr = (struct sockaddr_in6*)address;
     char ip[128];
     inet_ntop(AF_INET6, &(conn_addr->sin6_addr), ip, 128);
+    peer new_peer = {.addr = *conn_addr,
+                     .timestamp = (int)time(NULL),
+                     .dir = IN,
+                     .bev = bev};
+
+    add_peer(&new_peer);
     // printf("%s %d\n", ip, conn_addr->sin6_port);
-    bufferevent_setcb(bev, read_cb, NULL, NULL, NULL);
+    bufferevent_setcb(bev, read_cb, NULL, NULL, &new_peer);
     bufferevent_enable(bev, EV_READ);
     char string[1024] = "server->host\n";
     bufferevent_write(bev, string, 1024);
 }
 
-void connect_node(struct event_base* base, int port, char address[]) {
+int connect_node(struct event_base* base, int port, char address[]) {
     int sockfd, connfd;
     struct sockaddr_in6 connaddr = create_addr(port, address);
     sockfd = socket(AF_INET6, SOCK_STREAM, 0);
@@ -64,28 +79,49 @@ void connect_node(struct event_base* base, int port, char address[]) {
     printf("State connection Node IP(%s) PORT(%d) : %s\n", address, port, strerror(errno));
     if (res_connect != 0) {
         printf("Connection with the server failed.\n");
-    } else {
-        char buff[1024] = "abcdef";
-        struct bufferevent* bev =
-            bufferevent_socket_new(base, sockfd, BEV_OPT_CLOSE_ON_FREE);
-        bufferevent_write(bev, buff, 1025);
+        return -1;
+    }
+    char buff[1024] = "abcdef";
+    struct bufferevent* bev =
+        bufferevent_socket_new(base, sockfd, BEV_OPT_CLOSE_ON_FREE);
+    if (bufferevent_write(bev, buff, 1025) == 0) {
+        peer new_peer = {.addr = connaddr,
+                         .dir = OUT,
+                         .timestamp = (int)time(NULL),
+                         .bev = bev};
+
+        add_peer(&new_peer);
         bufferevent_enable(bev, EV_READ);
-        bufferevent_setcb(bev, read_cb, NULL, NULL, NULL);
-        /** char ip[128];
-         inet_ntop(AF_INET6, &(connaddr.sin6_addr), ip, 128);
-         struct VersionMessage* version =
-             version_msg_new(ip, connaddr.sin6_port);
-         char buff[1024];
-         char string[] = "hello";
-         char final[65];
-         double_sha256(string, final);
-                  version_to_string(version, buff);
-         bufferevent_write(bev, buff, 1025); **/
+        bufferevent_setcb(bev, read_cb, NULL, NULL, &new_peer);
+        return 0;
+    };
+    return -1;
+    /** char ip[128];
+     inet_ntop(AF_INET6, &(connaddr.sin6_addr), ip, 128);
+     struct VersionMessage* version =
+         version_msg_new(ip, connaddr.sin6_port);
+     char buff[1024];
+     char string[] = "hello";
+     char final[65];
+     double_sha256(string, final);
+              version_to_string(version, buff);
+     bufferevent_write(bev, buff, 1025); **/
+}
+
+void* event_thread(void* arg) {
+    struct event_base* base = arg;
+    event_base_dispatch(base);
+    return 0;
+}
+
+void* get_in(void* arg) {
+    while (1) {
+        printf("%d\n", peer_num);
+        sleep(2);
     }
 }
 
 void node_init(int serv_port) {
-    evthread_use_pthreads();
     struct event_base* base = event_base_new();
     struct evconnlistener* listener;
     struct sockaddr_in6 servaddr = create_addr(serv_port, "::1");
@@ -100,8 +136,17 @@ void node_init(int serv_port) {
          fprintf(fp, "%d\n", PORT);
          fclose(fp);**/
     }
+
     connect_node(base, (serv_port != 3306 ? 3306 : 4000), "::1");
     evconnlistener_set_error_cb(listener, error_cb);
-    event_base_dispatch(base);
+
+    pthread_t id[5];
+    int ret_thread[5];
+    //
+    pthread_create(&id[0], NULL, event_thread, base);
+    pthread_create(&id[1], NULL, get_in, NULL);
+
+    pthread_join(id[0], (void**)&ret_thread[0]);
+    pthread_join(id[1], (void**)&ret_thread[0]);
 }
 
--- src/network.h
+++ src/network.h
@@ -1,11 +1,29 @@
 #ifndef NETWORK_H
 #define NETWORK_H
+#include <event2/bufferevent.h>
+#include <event2/listener.h>
+#include <sys/socket.h>
+#include <time.h>
+
+#define SA struct sockaddr
+#define MAX_OUT_CONNECTION 10
+
+enum conn_dir { IN,
+                OUT };
+
+typedef struct {
+    struct sockaddr_in6 addr;
+    int64_t timestamp;
+    enum conn_dir dir;
+    struct bufferevent* bev;
+} peer;
 
 struct sockaddr_in6 create_addr(int port, char* address);
 void error_cb(struct evconnlistener* listener, void* ctx);
 void read_cb(struct bufferevent* bev, void* ctx);
 void accept_conn_cb(struct evconnlistener* listener, evutil_socket_t fd,
                     struct sockaddr* address, int socklen, void* ctx);
-void connect_node(struct event_base* base, int port, char address[]);
+int connect_node(struct event_base* base, int port, char address[]);
 void node_init(int serv_port);
+void* event_thread(void* arg);
 #endif
